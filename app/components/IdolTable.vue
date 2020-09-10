d02062a3c49093cd56bf38b8cece3116e89bf125
Haniel Austria <hanniiel@live.com.mx>
2020-09-10 01:58:17 -0500

    infinite scroll idols

--- app/components/IdolTable.vue
+++ app/components/IdolTable.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="table-wrapper-scroll-y my-custom-scrollbar">
+        <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
             <table class="table table-bordered table-striped mb-0">
                 <thead>
                 <tr>
@@ -12,7 +12,7 @@
                 <tr :key="idol._id" v-for="idol in idols">
                     <th scope="row">{{idol.hangul}}</th>
                     <td>{{idol.name}}</td>
-                    <td><img :src="idol.avatar" class="img-thumbnail" style="width:20%"> </td>
+                    <td><img :src="idol.avatar" class="img-thumbnail" style="height:150" > </td>
                 </tr>
                 </tbody>
             </table>
@@ -27,12 +27,28 @@ export default {
     data(){
         return{
             idols : [],
+            limit: 10,
+            page:0,
+            busy: false
         }
     },
+    methods:{
+        loadMore(){
+            this.busy = true;
+             axios
+            .get(`https://evening-savannah-98320.herokuapp.com/api/idol?per_page=${this.limit}&page=${this.page}`)
+            .then(response => {
+                this.idols = this.idols.concat(response.data);
+                this.page +=1;
+                this.busy = false;
+            })
+        }
+    },
+    created(){
+        this.loadMore();
+    },
     mounted(){
-        axios
-            .get('https://evening-savannah-98320.herokuapp.com/api/idol')
-            .then(response => (this.idols = response.data))
+       
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import IdolTable from './components/IdolTable.vue'
 
+var infiniteScroll =  require('vue-infinite-scroll');
+Vue.use(infiniteScroll);
+
 new Vue({
     el:"#app2",
     render:h=>h(App),
