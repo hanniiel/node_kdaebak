b949f372b7714e13021f8fae565d9f69baeab78e
Haniel Austria <hanniiel@live.com.mx>
2023-01-31 14:44:46 -0600

    cert

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 require("dotenv").config();
-const express= require("express");
-const cors = require('cors');
-const history = require('connect-history-api-fallback');
+const express = require("express");
+const cors = require("cors");
+const history = require("connect-history-api-fallback");
 //firebase admin impl
 const admin = require("firebase-admin");
-
-admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
-  databaseURL: "https://kdaebakapp.firebaseio.com"
+import { initializeApp, cert } from "firebase-admin/app";
+initializeApp({
+  credential: cert(config.GOOGLE_APPLICATION_CREDENTIALS),
+  databaseURL: "https://kdaebakapp.firebaseio.com",
 });
+
 //
-require(__dirname+"/models/connection");
-const idolRouter = require(__dirname+ "/routers/idol");
-const groupRouter = require(__dirname+"/routers/group");
-const userRouter = require(__dirname+"/routers/user");
-const voteRouter = require(__dirname+"/routers/vote");
-const faveRotuer = require(__dirname+'/routers/favorite');
+require(__dirname + "/models/connection");
+const idolRouter = require(__dirname + "/routers/idol");
+const groupRouter = require(__dirname + "/routers/group");
+const userRouter = require(__dirname + "/routers/user");
+const voteRouter = require(__dirname + "/routers/vote");
+const faveRotuer = require(__dirname + "/routers/favorite");
 
-const upload = require(__dirname+"/middleware/upload");
-
+const upload = require(__dirname + "/middleware/upload");
 
 const app = express();
 //middleware
-app.use(history({
-  rewrites:[
-    {
-      from:/\/api/,
-      to:function(context){
-        return context.parsedUrl.pathname;
-      }
-    }
-  ]
-}));
+app.use(
+  history({
+    rewrites: [
+      {
+        from: /\/api/,
+        to: function (context) {
+          return context.parsedUrl.pathname;
+        },
+      },
+    ],
+  })
+);
 app.use(cors());
 app.use(express.json());
 
@@ -42,16 +44,15 @@ app.use(voteRouter);
 app.use(faveRotuer);
 app.use(express.static("public"));
 
-app.post("/upload",upload, async function(req,res){
-  try{
-    if(req.file==null){
-      return res.status(400).send({error:"no file specified"});
+app.post("/upload", upload, async function (req, res) {
+  try {
+    if (req.file == null) {
+      return res.status(400).send({ error: "no file specified" });
     }
-    res.send({link:req.file.data.link});
-
-  }catch(error){
+    res.send({ link: req.file.data.link });
+  } catch (error) {
     console.log(error);
-    res.status(400).send('error');
+    res.status(400).send("error");
   }
 });
 
@@ -60,6 +61,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port,function(){
+app.listen(port, function () {
   console.log("server started");
 });
