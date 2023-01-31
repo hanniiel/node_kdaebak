70fe091c51f768622608c6015039d30a6413a13e
Haniel Austria <hanniiel@live.com.mx>
2023-01-31 14:39:01 -0600

    Revert "updates to netlify & local"
    This reverts commit 61452d18fbe446dc531079a1540eab7cd8044b4e.

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
 require("dotenv").config();
-const express = require("express");
-const serverless = require("serverless-http");
-const cors = require("cors");
-const history = require("connect-history-api-fallback");
+const express= require("express");
+const cors = require('cors');
+const history = require('connect-history-api-fallback');
 //firebase admin impl
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-  ),
-  databaseURL: "https://kdaebakapp.firebaseio.com",
+  credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)),
+  databaseURL: "https://kdaebakapp.firebaseio.com"
 });
 //
-require(__dirname + "/models/connection");
-const idolRouter = require(__dirname + "/routers/idol");
-const groupRouter = require(__dirname + "/routers/group");
-const userRouter = require(__dirname + "/routers/user");
-const voteRouter = require(__dirname + "/routers/vote");
-const faveRotuer = require(__dirname + "/routers/favorite");
+require(__dirname+"/models/connection");
+const idolRouter = require(__dirname+ "/routers/idol");
+const groupRouter = require(__dirname+"/routers/group");
+const userRouter = require(__dirname+"/routers/user");
+const voteRouter = require(__dirname+"/routers/vote");
+const faveRotuer = require(__dirname+'/routers/favorite');
 
-const upload = require(__dirname + "/middleware/upload");
+const upload = require(__dirname+"/middleware/upload");
+
 
 const app = express();
 //middleware
-app.use(
-  history({
-    rewrites: [
-      {
-        from: /\/api/,
-        to: function (context) {
-          return context.parsedUrl.pathname;
-        },
-      },
-    ],
-  })
-);
+app.use(history({
+  rewrites:[
+    {
+      from:/\/api/,
+      to:function(context){
+        return context.parsedUrl.pathname;
+      }
+    }
+  ]
+}));
 app.use(cors());
 app.use(express.json());
 
@@ -46,15 +42,16 @@ app.use(voteRouter);
 app.use(faveRotuer);
 app.use(express.static("public"));
 
-app.post("/upload", upload, async function (req, res) {
-  try {
-    if (req.file == null) {
-      return res.status(400).send({ error: "no file specified" });
+app.post("/upload",upload, async function(req,res){
+  try{
+    if(req.file==null){
+      return res.status(400).send({error:"no file specified"});
     }
-    res.send({ link: req.file.data.link });
-  } catch (error) {
+    res.send({link:req.file.data.link});
+
+  }catch(error){
     console.log(error);
-    res.status(400).send("error");
+    res.status(400).send('error');
   }
 });
 
@@ -63,5 +60,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-module.exports.app = app;
-module.exports.handler = serverless(app);
+app.listen(port,function(){
+  console.log("server started");
+});
--- local.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(port, function () {
-  console.log("server started");
-});
